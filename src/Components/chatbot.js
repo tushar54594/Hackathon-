17a417529a36e0d60ed292e4cb1c17077c4a4933
Tushar Pathak <tusharpathak2503@gmail.com>
2022-11-25 14:27:32 +0530

    third commit

--- src/Components/chatbot.js
+++ src/Components/chatbot.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import './chatbot.css'
+import './registration.scss'
 
 function Chatbot() {
+    // const [isVisible, setIsVisible] = useState(true);
+
+    // const handleClick = event => {
+    //   setIsVisible(current => !current);
+    // };
+
   return (
-    <div>
-      
+    <div className="chatbot">
+      <div className="chat-heading">
+        <span>Health Diagnosis </span>
+      </div>
+      <div className="chat-body">
+        <div className="chat-output"></div>
+        <input type="text" className="chat-input" placeholder="Enter your symptoms"/>
+        <i class="fa fa-paper-plane" aria-hidden="true"></i>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chatbot
+export default Chatbot;
